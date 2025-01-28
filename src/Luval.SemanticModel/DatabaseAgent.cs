597bd125bbd18f6cf5df4011490203dc22b4fd94
Oscar Marin <oscar@marin.cr>
2025-01-27 20:38:34 -0600

    improvements

--- src/Luval.SemanticModel/DatabaseAgent.cs
+++ src/Luval.SemanticModel/DatabaseAgent.cs
@@ -12,58 +12,42 @@ using Microsoft.Extensions.Configuration;
 using System.Text.Json;
 using Luval.GenAIBotMate.Core.Services;
 using Luval.GenAIBotMate.Infrastructure.Interfaces;
+using Luval.SemanticModel.PlugIns;
 
 namespace Luval.SemanticModel
 {
     public class DatabaseAgent : GenAIBotService
     {
-        private Kernel _kernel = default!;
-        private readonly IChatCompletionService _chatCompletionService;
-        private readonly OpenAIPromptExecutionSettings _settings;
-        private readonly ILogger _logger;
-        private ChatHistory _chatHistory;
-        private readonly Catalog _catalog;
+        private static Kernel _kernel = default!;
 
         public DatabaseAgent(ILoggerFactory loggerFactory, IConfiguration configuration, IGenAIBotStorageService storageService, IMediaService mediaService)
+            : base(GetChatCompletions(configuration,loggerFactory),
+                    storageService, mediaService, loggerFactory.CreateLogger<GenAIBotService>())
         {
-            _logger = loggerFactory.CreateLogger("App");
-            var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion("gpt-4o", configuration["OpenAIKey"] ?? throw new InvalidOperationException("OpenAIKey setting not provided in configuration"));
-            _kernel = builder.Build();
-            _chatCompletionService = _kernel.GetRequiredService<IChatCompletionService>();
-            _settings = new OpenAIPromptExecutionSettings()
-            {
-                Temperature = 0
-            };
-            _catalog = GetCatalog();
-            _chatHistory = new ChatHistory();
-
-            _chatHistory.AddSystemMessage(GetSystemPrompt());
+            SetKernel(_kernel);
         }
 
         private static IChatCompletionService GetChatCompletions(IConfiguration configuration, ILoggerFactory loggerFactory)
         {
             var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion("gpt-4o", configuration["OpenAIKey"] ?? throw new InvalidOperationException("OpenAIKey setting not provided in configuration"));
-            builder.Plugins.AddFromObject(plugIn, "DatabasePlugIn");
+            builder.Plugins.AddFromObject(new SqlServerPlugIn(configuration, loggerFactory.CreateLogger("App")), "DatabasePlugIn");
             var kernel = builder.Build();
             _kernel = kernel;
             return kernel.GetRequiredService<IChatCompletionService>();
         }
 
-        protected virtual string GetSystemPrompt()
+        public static string GetSystemPrompt()
         {
+            var catalog = GetCatalog();
             var p = File.ReadAllText(".\\Prompts\\system-prompt-1.txt");
-            var sp = p.Replace("<<yaml>>", _catalog.ToYAML());
+            var sp = p.Replace("<<yaml>>", catalog.ToYAML());
             return sp;
         }
 
-        protected virtual Catalog GetCatalog()
+        protected static Catalog GetCatalog()
         {
             return JsonSerializer.Deserialize<Catalog>(File.ReadAllText("full-catalog.json")) ?? 
                 throw new InvalidOperationException("Unable to extract catalog");
         }
-    }
-
-    public class DatabaseAgentConfiguration
-    {
     }
 }
--- src/Semantic.UI/Program.cs
+++ src/Semantic.UI/Program.cs
@@ -1,6 +1,9 @@
 using Luval.AuthMate.Infrastructure.Logging;
 using Luval.GenAIBotMate.Infrastructure.Configuration;
 using Luval.GenAIBotMate.Infrastructure.Data;
+using Luval.GenAIBotMate.Infrastructure.Interfaces;
+using Luval.SemanticModel;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Semantic.UI.Components;
 
@@ -18,11 +21,15 @@ namespace Semantic.UI
                 .AddInteractiveServerComponents();
             builder.Services.AddFluentUIComponents();
 
+            builder.Services.AddLogging();
+
             //TODO: Add secrests for the OpenAI key, Azure Blob Storage connection string
             builder.Services.AddGenAIBotServicesDefault(
                 config.GetValue<string>("OpenAIKey") ?? throw new ArgumentNullException("OpenAIKey not found in secrets.json"),
                 config.GetValue<string>("AzureStorageConnString") ?? throw new ArgumentNullException("AzureStorageConnString not found in secrets.json")
             );
+
+            builder.Services.AddScoped<DatabaseAgent>();
 
             var app = builder.Build();
 
@@ -44,12 +51,29 @@ namespace Semantic.UI
 
             // Initialize the database
             var context = new SqliteChatDbContext();
-            var dbHelper = new GenAIBotContextHelper(context, new ColorConsoleLogger<GenAIBotContextHelper>());
+            var dbHelper = new GenAIBotContextHelper(context, new Luval.AuthMate.Infrastructure.Logging.ColorConsoleLogger<GenAIBotContextHelper>());
             dbHelper.InitializeAsync()
                 .GetAwaiter()
                 .GetResult();
 
+            // Check if the bot has a system prompt
+            CheckBot(context);
+
             app.Run();
+        }
+
+        private static void CheckBot(IChatDbContext context)
+        {
+            var bot = context.GenAIBots.FirstOrDefault();
+            if (bot == null) return;
+            if (string.IsNullOrEmpty(bot.SystemPrompt))
+            {
+                bot.SystemPrompt = DatabaseAgent.GetSystemPrompt();
+                bot.UtcUpdatedOn = DateTime.UtcNow;
+                bot.UpdatedBy = "System";
+                bot.Version++;
+                context.SaveChanges();
+            }
         }
     }
 }
